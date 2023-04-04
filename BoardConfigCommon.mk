18b25f5f61abc9ead3e0382f7992a9ae2f2da44b
IQ7 <iq7.dev@gmail.com>
2023-04-04 16:03:51 +0700

    sdm660-common: Set TARGET_KERNEL_LLVM_BINUTILS to false * ld.lld: error: target emulation unknown: -m or at least one .o file required

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -67,6 +67,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/xiaomi/whyred
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_LLVM_BINUTILS := false
 
 #TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin
 #TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
